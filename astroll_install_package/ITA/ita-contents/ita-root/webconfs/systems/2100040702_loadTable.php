66c336111ca8d70856918aa9893fdaf11fe92a2e
Your Name <you@example.com>
2019-06-04 17:39:04 +0900

    2019/06/04 17:06:04

--- astroll_install_package/ITA/ita-contents/ita-root/webconfs/systems/2100040702_loadTable.php
+++ astroll_install_package/ITA/ita-contents/ita-root/webconfs/systems/2100040702_loadTable.php
@@ -16,7 +16,7 @@
 //////////////////////////////////////////////////////////////////////
 //
 //  【処理概要】
-//    ・Ansible(Legacy Role)インターフェース情報 
+//    ・Ansibleインターフェース情報 
 //
 //////////////////////////////////////////////////////////////////////
 $tmpFx = function (&$aryVariant=array(),&$arySetting=array()){
@@ -25,7 +25,7 @@ $tmpFx = function (&$aryVariant=array(),&$arySetting=array()){
     $arrayWebSetting = array();
     $arrayWebSetting['page_info'] = $g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202020");
 /*
-Ansible(Legacy Role)インターフェース情報
+Ansibleインターフェース情報
 */
     $tmpAry = array(
         'TT_SYS_01_JNL_SEQ_ID'=>'JOURNAL_SEQ_NO',
@@ -40,17 +40,11 @@ Ansible(Legacy Role)インターフェース情報
         'TT_SYS_NDB_LUP_TIME_ID'=>'UPD_UPDATE_TIMESTAMP'
     );
 
-    $table = new TableControlAgent('D_ANSIBLE_LRL_IF_INFO','ANSIBLE_IF_INFO_ID', $g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202030"), 'D_ANSIBLE_LRL_IF_INFO_JNL', $tmpAry);
+    $table = new TableControlAgent('B_ANSIBLE_IF_INFO','ANSIBLE_IF_INFO_ID', $g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202030"), ' B_ANSIBLE_IF_INFO_JNL', $tmpAry);
     $tmpAryColumn = $table->getColumns();
     $tmpAryColumn['ANSIBLE_IF_INFO_ID']->setSequenceID('B_ANSIBLE_IF_INFO_RIC');
     $tmpAryColumn['JOURNAL_SEQ_NO']->setSequenceID('B_ANSIBLE_IF_INFO_JSQ');
     unset($tmpAryColumn);
-
-    // ----VIEWをコンテンツソースにする場合、構成する実体テーブルを更新するための設定
-    $table->setDBMainTableHiddenID('B_ANSIBLE_IF_INFO');
-    $table->setDBJournalTableHiddenID('B_ANSIBLE_IF_INFO_JNL');
-    // 利用時は、更新対象カラムに、「$c->setHiddenMainTableColumn(true);」を付加すること
-    // VIEWをコンテンツソースにする場合、構成する実体テーブルを更新するための設定----
 
     // QMファイル名プレフィックス
     $table->setDBMainTableLabel($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202040"));
@@ -61,121 +55,149 @@ Ansible(Legacy Role)インターフェース情報
     $table->setGeneObject('AutoSearchStart',true);  //('',true,false)
     // 検索機能の制御----
 
-
-
-	$objVldt = new SingleTextValidator(1,256,false);
-    $c = new TextColumn('ANSIBLE_STORAGE_PATH_LNX',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202060"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202070"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-	$c->setValidator($objVldt);
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
-
-	$objVldt = new SingleTextValidator(1,256,false);
-    $c = new TextColumn('ANSIBLE_STORAGE_PATH_ANS',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202080"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202090"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-	$c->setValidator($objVldt);
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
-
-	$objVldt = new SingleTextValidator(1,256,false);
-    $c = new TextColumn('SYMPHONY_STORAGE_PATH_ANS',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202095"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202096"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-	$c->setValidator($objVldt);
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
-
-	$objVldt = new SingleTextValidator(1,8,false);
-    $c = new TextColumn('ANSIBLE_PROTOCOL',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203010"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203020"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-	$c->setValidator($objVldt);
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
-
-	$objVldt = new SingleTextValidator(1,128,false);
+    /* ホスト名 */
+    $objVldt = new SingleTextValidator(1,128,false);
     $c = new TextColumn('ANSIBLE_HOSTNAME',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203030"));
     $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203040"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-	$c->setValidator($objVldt);
+    $c->setValidator($objVldt);
     $c->setRequired(true);//登録/更新時には、入力必須
     $table->addColumn($c);
 
+    /* プロトコル */
+    $objVldt = new SingleTextValidator(1,8,false);
+    $c = new TextColumn('ANSIBLE_PROTOCOL',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203010"));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203020"));//エクセル・ヘッダでの説明
+    $c->setValidator($objVldt);
+    $c->setRequired(true);//登録/更新時には、入力必須
+    $table->addColumn($c);
+
+    /* ポート */
     $c = new NumColumn('ANSIBLE_PORT',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203050"));
     $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203060"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
     $c->setSubtotalFlag(false);
-	$c->setValidator(new IntNumValidator(null,null));
+    $c->setValidator(new IntNumValidator(null,null));
     $c->setRequired(true);//登録/更新時には、入力必須
     $table->addColumn($c);
 
-    /////////////////////////////////////////////////
-    // Ansible実行時のオプションパラメータ
-    /////////////////////////////////////////////////
+    /* 実行区分 */
+    $c = new IDColumn('ANSIBLE_EXEC_MODE',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203065"),'B_ANSIBLE_EXEC_MODE','ID','NAME','', array('OrderByThirdColumn'=>'ID'));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203066"));
+    $c->setRequired(true);
+    $table->addColumn($c);
+
+    /* データリレイストレージパス(ITA) */
+    $objVldt = new SingleTextValidator(1,256,false);
+    $c = new TextColumn('ANSIBLE_STORAGE_PATH_LNX',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202060"));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202070"));//エクセル・ヘッダでの説明
+    $c->setValidator($objVldt);
+    $c->setRequired(true);//登録/更新時には、入力必須
+    $table->addColumn($c);
+
+    /* データリレイストレージパス(Ansible/Tower) */
+    $objVldt = new SingleTextValidator(1,256,false);
+    $c = new TextColumn('ANSIBLE_STORAGE_PATH_ANS',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202080"));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202090"));//エクセル・ヘッダでの説明
+    $c->setValidator($objVldt);
+    $c->setRequired(true);//登録/更新時には、入力必須
+    $table->addColumn($c);
+
+    /* Symphonyデータリレイストレージパス(Ansible/Tower) */
+    $objVldt = new SingleTextValidator(1,256,false);
+    $c = new TextColumn('SYMPHONY_STORAGE_PATH_ANS',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202095"));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1202096"));//エクセル・ヘッダでの説明
+    $c->setValidator($objVldt);
+    $c->setRequired(true);//登録/更新時には、入力必須
+    $table->addColumn($c);
+    
+    /* Ansible-Playbook実行時のオプションパラメータ */
     $objVldt = new SingleTextValidator(0,512,false);
     $c = new TextColumn('ANSIBLE_EXEC_OPTIONS',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204015"));
     $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204016"));
-
-    $c->setHiddenMainTableColumn(true);
-
     $objVldt = new SingleTextValidator(0,512,false);
     $c->setValidator($objVldt);
-
-    // 必須入力ではない
     $c->setRequired(false);
-
     $table->addColumn($c);
 
+    // ----- Ansible情報
+    $acg = new ColumnGroup($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-9010000004"));
+
+        /* 認証キー Key */
+        $objVldt = new SingleTextValidator(1,64,false);
+        $c = new TextColumn('ANSIBLE_ACCESS_KEY_ID',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203070"));
+        $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203080"));//エクセル・ヘッダでの説明
+        $c->setValidator($objVldt);
+        $c->setRequired(true);//登録/更新時には、入力必須
+        $acg->addColumn($c);
+
+        /* 認証キー Value */
+        $objVldt = new SingleTextValidator(1,64,false);
+        $c = new PasswordColumn('ANSIBLE_SECRET_ACCESS_KEY',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203090"));
+        $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204010"));//エクセル・ヘッダでの説明
+        $c->setValidator($objVldt);
+        $c->setRequired(true);//登録/更新時には、入力必須
+        $c->setUpdateRequireExcept(1);//1は空白の場合は維持、それ以外はNULL扱いで更新
+        $c->setEncodeFunctionName("ky_encrypt");
+        $acg->addColumn($c);
+
+    $table->addColumn($acg);
+    // Ansible情報 -----
+
+
+    // ----- Ansible Tower情報
+    $tcg = new ColumnGroup($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-9010000005"));
+
+        /* 組織         */
 	$objVldt = new SingleTextValidator(1,64,false);
-    $c = new TextColumn('ANSIBLE_ACCESS_KEY_ID',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203070"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203080"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-	$c->setValidator($objVldt);
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
+	$c = new TextColumn('ANSTWR_ORGANIZATION',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-9010000002"));
+	$c -> setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-9010000003")); // エクセル・ヘッダでの説明
+	$c -> setValidator($objVldt);
+	$c -> setRequired(true); // 登録/更新時には、入力必須
+	$tcg->addColumn($c);
 
-	$objVldt = new SingleTextValidator(1,64,false);
-    $c = new PasswordColumn('ANSIBLE_SECRET_ACCESS_KEY',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1203090"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204010"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);
-	$c->setValidator($objVldt);
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $c->setUpdateRequireExcept(1);//1は空白の場合は維持、それ以外はNULL扱いで更新
-    $c->setEncodeFunctionName("ky_encrypt");
-    $table->addColumn($c);
+	/* 接続トークン */
+	$objVldt = new SingleTextValidator(1,256,false);
+	$c = new PasswordColumn('ANSTWR_AUTH_TOKEN',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-9010000000"));
+	$c -> setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-9010000001")); // エクセル・ヘッダでの説明
+	$c -> setValidator($objVldt);
+	$c -> setRequired(true); // 登録/更新時には、入力必須
+	$c -> setUpdateRequireExcept(1); // 1は空白の場合は維持、それ以外はNULL扱いで更新
+    	$c -> setEncodeFunctionName("ky_encrypt");
+	$tcg->addColumn($c);
 
-    $c = new NumColumn('ANSIBLE_REFRESH_INTERVAL',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204020"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204030"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-    $c->setSubtotalFlag(false);
-	$c->setValidator(new IntNumValidator(1000,null));
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
+	/* 実行時データ削除 */
+	$c = new IDColumn('ANSTWR_DEL_RUNTIME_DATA',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-907390509"),'B_ANS_TWER_RUNDATA_DEL_FLAG','FLAG_ID','FLAG_NAME','');
+	$c -> setDescription($g['objMTS']->getSomeMessage('ITAANSIBLEH-MNU-907350509'));//エクセル・ヘッダでの説明
+	$c -> setJournalTableOfMaster('B_ANSTWER_RUNDATA_DEL_FLAG_JNL');
+	$c -> setJournalSeqIDOfMaster('JOURNAL_SEQ_NO');
+	$c -> setJournalLUTSIDOfMaster('LAST_UPDATE_TIMESTAMP');
+	$c -> setJournalKeyIDOfMaster('FLAG_ID');
+	$c -> setJournalDispIDOfMaster('FLAG_NAME');
+	$tcg -> addColumn($c);
 
-    $c = new NumColumn('ANSIBLE_TAILLOG_LINES',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204040"));
-    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204050"));//エクセル・ヘッダでの説明
-    $c->setHiddenMainTableColumn(true);//コンテンツのソースがヴューの場合、登録/更新の対象とする際に、trueとすること。setDBColumn(true)であることも必要。
-    $c->setSubtotalFlag(false);
-	$c->setValidator(new IntNumValidator(null,null));
-    $c->setRequired(true);//登録/更新時には、入力必須
-    $table->addColumn($c);
+    $table -> addColumn($tcg);
+    // Ansible Tower情報 -----
 
-    ////////////////////////////////////////////////////////////////////
-    // パラメータシートの具体値がNULLでも代入値管理に登録するかのフラグ
-    ////////////////////////////////////////////////////////////////////
+    /* パラメータシートの具体値がNULLでも代入値管理に登録するかのフラグ */
     $c = new IDColumn('NULL_DATA_HANDLING_FLG',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-6000000"),'B_VALID_INVALID_MASTER','FLAG_ID','FLAG_NAME','', array('OrderByThirdColumn'=>'FLAG_ID'));
     $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-6000002"));
-    $c->setHiddenMainTableColumn(true); //更新対象カラム
-
     $c->setRequired(true);
-
-    //コンテンツのソースがヴューの場合、登録/更新の対象とする
-    $c->setHiddenMainTableColumn(true);
-
     $table->addColumn($c);
 
+    /* 状態監視周期(単位ミリ秒) */
+    $c = new NumColumn('ANSIBLE_REFRESH_INTERVAL',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204020"));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204030"));//エクセル・ヘッダでの説明
+    $c->setSubtotalFlag(false);
+    $c->setValidator(new IntNumValidator(1000,null));
+    $c->setRequired(true);//登録/更新時には、入力必須
+    $table->addColumn($c);
+
+    /* 進行状態表示行数 */
+    $c = new NumColumn('ANSIBLE_TAILLOG_LINES',$g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204040"));
+    $c->setDescription($g['objMTS']->getSomeMessage("ITAANSIBLEH-MNU-1204050"));//エクセル・ヘッダでの説明
+    $c->setSubtotalFlag(false);
+    $c->setValidator(new IntNumValidator(null,null));
+    $c->setRequired(true);//登録/更新時には、入力必須
+    $table->addColumn($c);
 
     $table->fixColumn();
 
