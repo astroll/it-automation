01900f7b7b70fae9724bb421af9d6364b9d0634d
Your Name <you@example.com>
2019-06-04 17:43:25 +0900

    2019/06/04 17:06:25

--- astroll_install_package/ITA/ita-contents/ita-root/libs/backyardlibs/ansible_driver/ky_ansible_common_setenv.php
+++ astroll_install_package/ITA/ita-contents/ita-root/libs/backyardlibs/ansible_driver/ky_ansible_common_setenv.php
@@ -16,9 +16,14 @@
     //////////////////////////////////////////////////////////////////////
     //
     //  【処理概要】
-    //    RedMineチケット#1033に伴いansibleモジュールの実行に必要な共通定数を定義
+    //    ansibleモジュールの実行に必要な共通定数を定義
     //
     //////////////////////////////////////////////////////////////////////
+    if (empty($root_dir_path)) {
+        $root_dir_temp = array();
+        $root_dir_temp = explode("ita-root", dirname(__FILE__));
+        $root_dir_path = $root_dir_temp[0] . "ita-root";
+    }
 
     // ドライバ識別子
     define("DF_LEGACY_DRIVER_ID"           ,"L");
@@ -36,14 +41,30 @@
     // テンプレートファイルからグローバル変数を取り出す場合の区分
     define("DF_HOST_TEMP_GBL_HED"          ,"TEMP_GBL_");
 
-    // ロールパッケージ管理 ロールパッケージファイル(ZIP)格納先ディレクトリ
+    // ITA側で管理している ロールパッケージ管理 ロールパッケージファイル(ZIP)格納先ディレクトリ
     define("DF_ROLE_PACKAGE_FILE_CONTENTS_DIR"  ,"/uploadfiles/2100020303/ROLE_PACKAGE_FILE");
     // ITA側で管理している legacy用 子playbookファイル格納先ディレクトリ
     $vg_legacy_playbook_contents_dir  = $root_dir_path . "/uploadfiles/2100020104/PLAYBOOK_MATTER_FILE";
-    // ITA側で管理している legacy用 テンプレートファイル格納先ディレクトリ
-    $vg_legacy_template_contents_dir = $root_dir_path . "/uploadfiles/2100020110/ANS_TEMPLATE_FILE";
     // ITA側で管理している pioneer用 対話ファイル格納先ディレクトリ
     $vg_pioneer_playbook_contents_dir = $root_dir_path . "/uploadfiles/2100020205/DIALOG_MATTER_FILE";
-    // ITA側で管理している pioneer用 テンプレートファイル格納先ディレクトリ
-    $vg_pioneer_template_contents_dir = $root_dir_path . "/uploadfiles/2100020216/ANS_TEMPLATE_FILE";
+
+    // ITA側で管理している copyファイル格納先ディレクトリ
+    $vg_copy_contents_dir = $root_dir_path . "/uploadfiles/2100040703/CONTENTS_FILE";
+    // ITA側で管理している テンプレートファイル格納先ディレクトリ
+    $vg_template_contents_dir = $root_dir_path . "/uploadfiles/2100040704/ANS_TEMPLATE_FILE";
+
+    // 実行エンジン
+    define("DF_EXEC_MODE_ANSIBLE"         ,'1');         // Ansibleで実行
+    define("DF_EXEC_MODE_TOWER"           ,'2');         // AnsibleTowerで実行
+
+    // AnsibleTower処理区分
+    define("DF_EXECUTION_FUNCTION"        ,'1');
+    define("DF_CHECKCONDITION_FUNCTION"   ,'2');
+    define("DF_DELETERESOURCE_FUNCTION"   ,'3');
+
+    // B_ANS_TWR_JOBTP_PROPERTY->PROPERTY_TYPE
+    define("DF_JobTemplateKeyValueProperty","1");
+    define("DF_JobTemplateVerbosityProperty","2");
+    define("DF_JobTemplatebooleanTrueProperty","3");
+    define("DF_JobTemplateExtraVarsProperty","4");
 ?>
